fc097bc7628cb34169cf76cedad231f4b073fd7d
Shutenjiro <136637687+Shutenjiro@users.noreply.github.com>
2026-03-08 19:41:23 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,30 @@
-const CACHE_NAME = 'v15-cache';
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['index.html'])));
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+const CACHE="relatorio-v1"
 
-});
+self.addEventListener("install",e=>{
+
+e.waitUntil(
+
+caches.open(CACHE).then(cache=>{
+return cache.addAll([
+"/",
+"index.html"
+])
+})
+
+)
+
+})
+
+self.addEventListener("fetch",e=>{
+
+e.respondWith(
+
+caches.match(e.request).then(res=>{
+
+return res || fetch(e.request)
+
+})
+
+)
+
+})
