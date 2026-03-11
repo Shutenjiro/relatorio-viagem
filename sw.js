c01321a2c5292afea454b1edce60b4a51809cf2d
Shutenjiro <136637687+Shutenjiro@users.noreply.github.com>
2026-03-11 08:37:14 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,21 @@
-const CACHE_NAME = 'relatorio-viagem-v8';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
+const CACHE_NAME = 'viagem-pro-v9';
+const assets = [
+  'index.html',
+  'manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/4345/4345573.png'
 ];
 
+// Instalação e Cache
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
   );
+  self.skipWaiting();
 });
 
+// Ativação e limpeza de caches antigos
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => {
@@ -23,9 +26,11 @@ self.addEventListener('activate', event => {
   );
 });
 
+// Estratégia: Tenta Rede, se falhar (offline), usa o Cache
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
